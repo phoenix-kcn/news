df81ae7b9022145438c0a97594bd385e39cd6555
Shahadat Hossain <sht.kcn@gmail.com>
2026-01-18 03:41:48 +0600

    user authentication

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,11 +2,14 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-    class Meta:
+    class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        fields = UserCreationForm.Meta.fields + (
+            'email',
+            'age',
+        )
         
 class CustomUserChangeForm(UserChangeForm):
-    class Meta:
+    class Meta(UserChangeForm.Meta):
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = ('username', 'email', 'age',)
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
 
 class UserModelTests(TestCase):
@@ -31,3 +32,28 @@ class UserModelTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+        
+        
+class SignUpViewTests(TestCase):
+    def test_url_exists_at_correct_location_signupview(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_singup_view_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/signup.html')
+        
+    def test_signup_form(self):
+        response = self.client.post(
+            reverse('signup'), {
+            'username': 'newuser',
+            'email': 'newuser@gmail.com',
+            'password1': 'complexpassword123',
+            'password2': 'complexpassword123',
+            }
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, "newuser")
+        self.assertEqual(get_user_model().objects.all()[0].email, "newuser@gmail.com")
